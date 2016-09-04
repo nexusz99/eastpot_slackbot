d7e67abf6f722cdcd06a39d4f6d89d920d7f2e25
nexusz99 <nexusz99@gmail.com>
2016-09-04 18:12:33 +0900

    Add miner status api

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import eastbot
-import eastbot.plugins.miner as miner
 # https://github.com/lins05/slackbot
 
 
@@ -7,8 +6,6 @@ def main():
     eastbot.sched.start()
     eastbot.bot.run()
 
-    if not miner.is_running():
-        miner.start_mining()
 
 if __name__ == "__main__":
     main()
--- eastbot/plugins/miner.py
+++ eastbot/plugins/miner.py
@@ -9,20 +9,20 @@ from eastbot import bot, sched
 def is_running():
     cmd = "ps ax | grep 'qtminer' | awk '{print $1}'"
     output = os.popen(cmd).read().count('\n')
-    if output == 2 or output > 3:
-        stop_mining()
+    if output == 2:
         return False
-    elif output == 3:
+    elif output > 2:
         return True
 
 
 @sched.scheduled_job('cron', hour=2, minute=00)
-def stop_mining():
+def stop_mining(force=False):
     cmd = "kill -9 $(ps ax | grep 'qtminer' | awk '{print $1}')"
     os.system(cmd)
     slack_client = getattr(bot, "_client", None)
     channel_id = slack_client.find_channel_by_name('ethereum')
-    slack_client.send_message(channel_id, "[Mining Alert] Mining stopped")
+    if not force:
+        slack_client.send_message(channel_id, "[Mining Alert] Mining stopped")
 
 
 @sched.scheduled_job('cron', hour=9, minute=00)
@@ -30,6 +30,7 @@ def start_mining():
     if is_running():
         msg = 'Mining already started'
     else:
+        stop_mining(True)
         cmd = "sh /home/eastpot/qtminer/qtminer.sh -s asia1.ethermine.org:14444 " \
               "-u 0x71694c8f184b2133dd080c3dcb8e726e00194687.khupot -G " \
               "> /dev/null 2>&1 &"
@@ -49,3 +50,12 @@ def bot_start_mining(message):
 @respond_to('^stop$', re.IGNORECASE)
 def bot_stop_mining(message):
     stop_mining()
+
+
+@respond_to('^status', re.IGNORECASE)
+def bot_mining_status(message):
+    if is_running():
+        msg = "Mining is running"
+    else:
+        msg = "Mining stopped"
+    message.send("[Miner Status] " + msg)
